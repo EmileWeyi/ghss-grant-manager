38fabac2da0189fd8edbe3476ec66082847fa793
CD3 Cameroon <cd3cameroon@gmail.com>
2025-12-31 14:40:10 +0000

    fix: update firebase api keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, getApp, FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+const firebaseConfig = {
+  apiKey: "AIzaSyAu316JtAihX3umk7DUSX-RRI7vyJdggaw",
+  authDomain: "ghss-grant-manager.firebaseapp.com",
+  projectId: "ghss-grant-manager",
+  storageBucket: "ghss-grant-manager.firebasestorage.app",
+  messagingSenderId: "906747744514",
+  appId: "1:906747744514:web:12740717331467d322f21b",
+  measurementId: "G-1JV282WWV2"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
